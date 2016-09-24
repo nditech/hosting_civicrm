e8be86a343abb9d112bfae2c0a3cdf7589214356
Mathieu Lutfy <mathieu@bidon.ca>
2016-09-24 17:34:16 -0400

    tests: SiteUtils: refactor code and fix json_decode.

--- tests/HostingCiviTest/Command/SiteUtils.php
+++ tests/HostingCiviTest/Command/SiteUtils.php
@@ -6,17 +6,30 @@ class SiteUtils extends \HostingCiviTest\Command {
   /**
    * Returns the CiviCRM version.
    */
-  public static function getCiviVerson($site) {
-    $site .= '.aegir.example.com';
-
-    $output = self::exec('drush @' . escapeshellcmd($site) . ' cvapi System.get --out=json', [], TRUE);
-
-    $info = parse_json($output);
+  public static function getCiviVersion($site) {
+    $info = self::getCiviSystemInfo($site);
 
     if (isset($info['civi']['version'])) {
       return $info['civi']['version'];
     }
 
-    throw new Exception("HostingCiviTest\Command::getCiviVerson: could not find CiviCRM version: " . $info);
+    throw new \Exception("HostingCiviTest\Command::getCiviVerson: could not find CiviCRM version: " . print_r($info, 1));
   }
+
+  /**
+   * Returns the CiviCRM system info.
+   */
+  public static function getCiviSystemInfo($site) {
+    $site .= '.aegir.example.com';
+
+    // Flush the drush cache to avoid 'civicrm must be enable' error.
+    self::exec('drush @' . escapeshellcmd($site) . ' cc drush');
+
+    // Run System.get API
+    $output = self::exec('drush @' . escapeshellcmd($site) . ' cvapi System.get --out=json', [], TRUE);
+    $info = json_decode($output, TRUE);
+
+    return $info['values'][0];
+  }
+
 }
